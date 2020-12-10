8e10c17a8015b4eef365a6c1662e91050f705c2a
kiya-star <tarikkuma38@gmail.com>
2020-12-10 08:14:14 +0300

    signup activity updated

--- app/src/main/java/com/example/test/signup.java
+++ app/src/main/java/com/example/test/signup.java
@@ -19,17 +19,11 @@ import com.hbb20.CountryCodePicker;
 
 public class signup extends AppCompatActivity {
 
-    private TextInputLayout userEnteredPhone;
-    private CountryCodePicker countryCodePicker;
-    public final static String NUMBER_TO_VERIFY="0";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        countryCodePicker = findViewById(R.id.country);
-        userEnteredPhone = findViewById(R.id.phone);
     }
 
     public void backtologin(View view) {
@@ -38,54 +32,11 @@ public class signup extends AppCompatActivity {
         finish();
     }
 
-    public void checkPhoneNumber(View view) {
 
-        //Check if connected to the internet
-        if (!isConnected(this)) {
-            showDialog();
-       }
-        String userEnteredPhonenumber = userEnteredPhone.getEditText().getText().toString();
-        String userPhoneNumber = "+" + countryCodePicker.getSelectedCountryCode() + userEnteredPhonenumber;
+    public void nextpage(View view) {
+        Intent intent = new Intent(getApplicationContext(), signup_next_page.class);
+        startActivity(intent);
 
-        Intent intent = new Intent(getApplicationContext(), verifyPhone.class);
-          intent.putExtra(NUMBER_TO_VERIFY, userPhoneNumber);
-          startActivity(intent);
-         Toast.makeText(getApplicationContext(), userPhoneNumber, Toast.LENGTH_LONG).show();
 
     }
-
-    private boolean isConnected(signup signup) {
-        ConnectivityManager connectivityManager = (ConnectivityManager) signup.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo wifiConnection = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        NetworkInfo mobileConnection = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-        if ((wifiConnection != null && wifiConnection.isConnected()) || (mobileConnection != null && mobileConnection.isConnected())) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private void showDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Please, check your internet connection")
-                .setCancelable(false)
-                .setPositiveButton("Try again", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        startActivity(new Intent((Settings.ACTION_WIFI_SETTINGS)));
-                    }
-                })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        startActivity(new Intent(getApplicationContext(), loginSignup.class));
-                        finish();
-                    }
-                });
-
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-
-    }
-
 }
